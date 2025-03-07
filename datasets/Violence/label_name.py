7869858ec81b3bb3b4ab46afb1c6899685cabf39
dabaobei1416 <goole984955336@gmail.com>
2025-03-07 14:31:35 +0800

    变更

---
+++ datasets/Violence/label_name.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Auth ： 逗逗班学Python
+博客园、知乎：逗逗班学Python
+Bilibili：逗逗班学Python
+面包多: 逗逗班学Python
+代码地址见以下博客中给出:
+https://space.bilibili.com/456667721
+https://www.cnblogs.com/deeppython
+
+@IDE ：PyCharm
+
+"""
+Chinese_name = {'violence': "暴力倾向"}
+Label_list = list(Chinese_name.values())
